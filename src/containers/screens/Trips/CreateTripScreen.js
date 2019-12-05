297e17b3c6832b8dc37335b2e9148a07fe52e259
Giancarlo <giancarlo.segier@student.howest.be>
2019-12-05 17:00:41 +0100

    trying to use a datepicker

--- src/containers/screens/Trips/CreateTripScreen.js
+++ src/containers/screens/Trips/CreateTripScreen.js
@@ -15,6 +15,7 @@ import androidUI from '../../../styles/ui.android.style.js';
 import iosUI from '../../../styles/ui.ios.style.js';
 import firebase from 'react-native-firebase';
 
+import DatePicker from 'react-native-datepicker';
 export class CreateTripScreen extends Component {
   constructor(props) {
     super(props);
@@ -27,6 +28,7 @@ export class CreateTripScreen extends Component {
     this.state = {
       trips: [],
       newTripTitle: '',
+      date: Date.now(),
       userId: firebase.auth().currentUser.uid,
     };
 
@@ -67,6 +69,39 @@ export class CreateTripScreen extends Component {
               this.state.newTripTitle === '' ? '' : this.state.newTripTitle
             }
             onChangeText={text => this.setState({newTripTitle: text})}
+          />
+
+          <DatePicker
+            style={{width: 200}}
+            date={() => {
+              const day = this.state.date.getDate();
+              const month = this.state.date.getMonth();
+              const year = this.state.date.getYear();
+              console.log(this.state.date);
+
+              return `${day}/${month}/${year}`;
+            }} //initial date from state
+            mode="date" //The enum of date, datetime and time
+            placeholder="select date"
+            format="DD-MM-YYYY"
+            minDate="01-01-2016"
+            maxDate="01-01-2019"
+            confirmBtnText="Confirm"
+            cancelBtnText="Cancel"
+            customStyles={{
+              dateIcon: {
+                position: 'absolute',
+                left: 0,
+                top: 4,
+                marginLeft: 0,
+              },
+              dateInput: {
+                marginLeft: 36,
+              },
+            }}
+            onDateChange={date => {
+              this.setState({date: date});
+            }}
           />
 
           <TouchableHighlight
